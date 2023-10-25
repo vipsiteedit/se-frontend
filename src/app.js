92d61157d5ce14295be3455f0c7a7b03c2f4d3fe
Sergey Schelkonogov <vipsiteedit@gmail.com>
2023-10-25 13:09:02 +0300

    fix bug in app.init

--- src/app.js
+++ src/app.js
@@ -61,8 +61,11 @@ app.login = (params) => {
 };
 
 app.init = () => {
-  let storage = JSON.parse(localStorage.getItem("market") || "{}");
-
+  let res = localStorage.getItem('market');
+  if (!res || res === undefined || res == 'undefined') {
+      res = '{}'
+  }
+  let storage = JSON.parse(res);
   if (storage && storage.domain) {
     app.config.project = storage.project;
     app.config.version = storage.version;
